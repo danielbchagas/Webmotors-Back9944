55bdf072bcd3695ab8a2960010f719335eb02cc9
Daniel <daniel.chagas@runtalent.it>
2021-03-17 23:51:37 -0300

    arrumando o código

---
+++ Webmotors.Back9944/Services/AdvertisingService.cs
@@ -0,0 +1,82 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webmotors.Back9944.Interfaces.Repositories;
+using Webmotors.Back9944.Interfaces.Services;
+using Webmotors.Back9944.Models;
+using Webmotors.Back9944.Services.Results;
+using Webmotors.Back9944.Validations;
+
+namespace Webmotors.Back9944.Services
+{
+    public class AdvertisingService : IAdvertisingService
+    {
+        private readonly AdvertisingValidation _validator;
+        private readonly IAdvertisingRepository _repository;
+        
+        public AdvertisingService(IAdvertisingRepository repository)
+        {
+            _validator = new AdvertisingValidation();
+            _repository = repository;
+        }
+
+        public async Task<Advertising> Get(int id) => await _repository.Get(id);
+
+        public async Task<IEnumerable<Advertising>> Get() => await _repository.Get();
+
+        public async Task<AdvertisingResult> Create(Advertising advertising)
+        {
+            ValidationResult validation = _validator.Validate(advertising);
+            int result = 0;
+
+            if (validation.IsValid)
+                result = await _repository.Create(advertising);
+
+            return new AdvertisingResult 
+            {
+                Errors = validation?.Errors.Select(e => e.ErrorMessage),
+                AffectedRows = result
+            };
+        }
+
+        public async Task<AdvertisingResult> Delete(Advertising advertising)
+        {
+            ValidationResult validation = _validator.Validate(advertising);
+            int result = 0;
+            
+            if (validation.IsValid)
+            {
+                result = await _repository.Delete(advertising);
+            }
+
+            return new AdvertisingResult
+            {
+                Errors = validation?.Errors.Select(e => e.ErrorMessage),
+                AffectedRows = result
+            };
+        }
+
+        public async Task<AdvertisingResult> Update(Advertising advertising)
+        {
+            ValidationResult validation = _validator.Validate(advertising);
+            int result = 0;
+
+            if (validation.IsValid)
+            {
+                result = await _repository.Update(advertising);
+            }
+
+            return new AdvertisingResult
+            {
+                Errors = validation?.Errors.Select(e => e.ErrorMessage),
+                AffectedRows = result
+            };
+        }
+
+        public void Dispose()
+        {
+            _repository.Dispose();
+        }
+    }
+}
---
+++ Webmotors.Back9944/Services/Results/AdvertisingResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Webmotors.Back9944.Services.Results
+{
+    public class AdvertisingResult
+    {
+        public int AffectedRows { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
