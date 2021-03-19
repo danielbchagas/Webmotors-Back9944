875e4f6f966736974e8c1965266788267b93f845
Daniel <daniel.chagas@runtalent.it>
2021-03-19 19:49:02 -0300

    finalizando

--- Webmotors.Back9944.Business/Services/AdvertisingService.cs
+++ Webmotors.Back9944.Business/Services/AdvertisingService.cs
@@ -6,6 +6,7 @@ using Webmotors.Back9944.Business.Interfaces.Repositories;
 using Webmotors.Back9944.Business.Interfaces.Services;
 using Webmotors.Back9944.Business.Models;
 using Webmotors.Back9944.Validations;
+using System;
 
 namespace Webmotors.Back9944.Business.Services
 {
@@ -13,13 +14,28 @@ namespace Webmotors.Back9944.Business.Services
     {
         private readonly AdvertisingValidation _validator;
         private readonly IAdvertisingRepository _repository;
+        private readonly IWebmotorsService _service;
         public IEnumerable<string> _errors;
         
-        public AdvertisingService(IAdvertisingRepository repository)
+        public AdvertisingService(IAdvertisingRepository repository, IWebmotorsService service)
         {
             _validator = new AdvertisingValidation();
             _repository = repository;
+            _service = service;
             _errors = new List<string>();
+        }
+
+        private async Task<Advertising> Change(Advertising advertising)
+        {
+            var marca = (await _service.GetMakers()).FirstOrDefault(c => c.Id == Convert.ToInt32(advertising.Marca));
+            var modelo = (await _service.GetModels(Convert.ToInt32(advertising.Marca))).FirstOrDefault(m => m.Id == Convert.ToInt32(advertising.Modelo));
+            var versao = (await _service.GetVersions(Convert.ToInt32(advertising.Modelo))).FirstOrDefault(v => v.Id == Convert.ToInt32(advertising.Versao));
+
+            advertising.Marca = marca.Name;
+            advertising.Modelo = modelo.Name;
+            advertising.Versao = versao.Name;
+
+            return advertising;
         }
 
         public async Task<Advertising> Get(int id) => await _repository.Get(id);
@@ -36,7 +52,9 @@ namespace Webmotors.Back9944.Business.Services
                 return;
             }
                 
-            await _repository.Create(advertising);
+            Advertising newAdvertising = await Change(advertising);
+
+            await _repository.Create(newAdvertising);
         }
 
         public async Task Update(Advertising advertising)
@@ -49,7 +67,9 @@ namespace Webmotors.Back9944.Business.Services
                 return;
             }
 
-            await _repository.Update(advertising);
+            Advertising newAdvertising = await Change(advertising);
+
+            await _repository.Update(newAdvertising);
         }
 
         public async Task Delete(Advertising advertising)
