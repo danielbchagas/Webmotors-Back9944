22c502cf9a608f6e2c7e0fd688261ee8ba878a48
Daniel <daniel.chagas@runtalent.it>
2021-04-05 14:29:56 -0300

    refatorando

---
+++ Tests/Webmotors.Back9944.App.Tests/Data_AdvertisingRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+using Webmotors.Back9944.Business.Interfaces.Repositories;
+using Xunit;
+
+namespace Webmotors.Back9944.App.Tests
+{
+    public class Data_AdvertisingRepository : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly IAdvertisingRepository _repository;
+
+        public Data_AdvertisingRepository(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+
+            var newScope = _factory.Services.CreateScope();
+            
+            _repository = newScope.ServiceProvider.GetRequiredService<IAdvertisingRepository>();
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            // Arrange
+
+            // Act
+            var result = await _repository.Get();
+
+            // Assert
+            Assert.True(result.Count() > 0);
+        }
+    }
+}
